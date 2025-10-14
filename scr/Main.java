1b52ae8b761d1949bd3a81cc83546831878cd119
Bulanov Ivan <vanya.bulanov.1996@mail.ru>
2025-10-14 21:17:09 +0300

    commit test1

---
+++ scr/Main.java
@@ -0,0 +1,13 @@
+package scr;
+
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+        }
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+}
