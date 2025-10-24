a290980a0ec7fa035736e88ad65d021944d48173
Bulanov Ivan <vanya.bulanov.1996@mail.ru>
2025-10-25 02:31:58 +0300

    commit test1

--- scr/Main.java
+++ scr/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("task1");
+        System.out.println("task2");
         
         
         
